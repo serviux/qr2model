9a1e84694864d23deba821c3cd189288b09f9b21
Serviux <jcorm714@proton.me>
2025-11-11 18:58:10 -0500

    added helper functions

--- app/geometry/vertex.py
+++ app/geometry/vertex.py
@@ -1,6 +1,7 @@
 """Helper classes containing information about vertices and the order in which they are generated in the Mesh Constructor"""
 from dataclasses import dataclass
 from enum import IntEnum
+from math import sqrt
 
 class VERTEX_ORDER(IntEnum):
     """An enumeration of vertices generated """
@@ -32,16 +33,25 @@ class Vertex:
 
     def __str__(self) -> str:
         return f"({self.x}, {self.y}, {self.z})"
+    
+    def __eq__(self, other: 'Vertex') -> bool:
+        if isinstance(other, Vertex):
+            return (self.x == other.x and self.y == other.y and self.z == other.z)
+        raise(NotImplemented("Object comparison for Vertex and type is not implemented"))
 
     def to_list(self) -> list:
         """Converts vertex to a list in order x,y,z"""
         return [self.x, self.y, self.z]
     
-    def is_below(self, other_vertex: 'Vertex') -> bool:
+    def check_geo_constraints(self, other_vertex: 'Vertex') -> bool:
         """
-        Check if the current vertex is below another given vertex.
+        Check if the current vertex is directly below another given vertex.
 
         :param other_vertex: The vertex to compare against.
         :return: True if this vertex's coordinates are all less than the other vertex's coordinates.
         """
-        return self.x < other_vertex.x and self.y < other_vertex.y and self.z < other_vertex.z
+        return self.x == other_vertex.x and self.y == other_vertex.y and self.z < other_vertex.z
+    
+    def dist_from_other(self, other_vertex: 'Vertex') -> float:
+        """Calculates the distance between two vertices"""
+        return sqrt((other_vertex.x-  self.x ) ** 2 + (other_vertex.y - self.y) ** 2 + (other_vertex.z - self.z) ** 2)
