8e78bc45ca3ca58e72a78784ec6539a7007ee67d
serviux <jcorm714@proton.me>
2025-12-10 15:04:09 -0500

    refactoring and validation

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-from .qr_constructor_3d import MeshConstructionParams
-from .qr_constructor_3d import QRGenerator3d 
-from .geometry.plane import Plane
-from .geometry.vertex import Vertex, VERTEX_ORDER
